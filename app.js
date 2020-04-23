d54196abc66bdfcb9a71e4aa798afb460bb8a2d8
rrbyn <robin@DESKTOP-Q79J1H3.localdomain>
2020-04-23 13:54:42 +0300

    palju asju

--- app.js
+++ app.js
@@ -4,20 +4,32 @@ const feedBackDiv = document.getElementById('feedback')
 const timerDiv = document.getElementById('timer')
 const questionDiv = document.getElementById('question')
 
+//Tegeleb .json file valimisega
 
-var selectedQuestions = "citys"
+
+// See muutuja otsustab milliseid küsimusi kasutada
+var selectedJson = "citys"
+
+
+var counter = 1
 getinfo()
 
 function getinfo(){
-    fetch('./key_value.json')
+    fetch('./json/' + selectedJson + '.json')
         .then((response) => {
         return response.json()
       })
       .then((data) => {
-        console.log(data)
-        randint = Math.floor(Math.random() * (Object.keys(data.citys).length - 1))
-        correctAnswer = (Object.values(data.citys)[randint])
-        questionDiv.innerHTML = '<div class="question">' + "Mis on " + Object.keys(data.citys)[randint] + " pealinn?" +  "</div>"                 
+        var selectedData = Object.values(data.meta)[1]
+        selectedData = data[selectedData]
+        max = Object.keys(selectedData).length
+        question = Object.values(data.meta)[0]
+        randint = Math.floor(Math.random() * max)
+        randQuestion = Object.keys(selectedData)[randint]
+        correctAnswer = (Object.values(selectedData)[randint])
+        question = question.replace("*", randQuestion)
+        
+        questionDiv.innerHTML = '<div class="question">' +  question +  "</div>"                 
     
       });
     }
@@ -25,15 +37,17 @@ answerArea.focus()
 answerArea.addEventListener('keydown', event => {
   console.log(event.keyCode)
   if (event.keyCode == 13) {
-    if (answerArea.value == correctAnswer) {
-    console.log("Correct")
-    feedBackDiv.innerHTML = "<div id=correct>" + "Õige vastus" + "</div>"
-  } else{
-    console.log("Incorrect")
-    console.log(answerArea.value)
-    console.log(correctAnswer)
-    feedBackDiv.innerHTML = "<div id=incorrect>" + "Vale vastus" + "</div>"
-  }
+    if (answerArea.value.toUpperCase() == correctAnswer.toUpperCase()) {
+      console.log("Correct")
+      feedBackDiv.innerHTML = "<div id=correct>" + "Õige vastus" + "</div>"
+    } else{
+      console.log("Incorrect")
+      console.log(answerArea.value)
+      console.log(correctAnswer)
+      feedBackDiv.innerHTML = "<div id=incorrect>" + "Vale vastus" + "</div>"
+      + '<div>' + "Õige vastus oli " + correctAnswer +  '</div>'
+    }
+  counter++
   getinfo()
   answerArea.value = ""
   
